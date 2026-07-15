a3e8137511c41b8608f4213e9f12a55e893718f8
hooray804 <profs_35_pointer@icloud.com>
2026-07-16 08:47:05 +0900

    Update lang_ko.php

--- Translations-Web/lang_ko.php
+++ Translations-Web/lang_ko.php
@@ -22,18 +22,18 @@ $content_download = "다운로드";
 $content_issues = "이슈";
 $content_help = "도움말";
 $content_forum = "포럼";
-$content_changelog = "히스토리";
+$content_changelog = "변경 사항";
 $content_title = "macOS 압축 프로그램";
 
 // Download
-$content_platform = "$version_replace 또는 신규 버전이 필요합니다.";
+$content_platform = "$version_replace 이상 버전이 필요합니다.";
 $content_download_text = "더 많은 파일을 더 안전하게 압축하고, 공유하세요";
 $content_download_love = "마음에 드신다면?";
 $content_download_legacy = "구 버전";
-$content_download_helper = "도움말";
+$content_download_helper = "도우미";
 $content_downloading = "다운로드 중: ";
 $content_get_platform = "설치 플랫폼: ";
-$content_testflight = "이 빌드를 설치하려면 TestFlight가 필요합니다.";
+$content_testflight = "이 빌드를 설치하려면 TestFlight이 필요합니다.";
 $content_downloading_if_fails = "다운로드가 자동으로 시작되지 않으면 ";
 $content_downloading_if_fails_click_here = "여기를 누르세요";
 
@@ -60,7 +60,7 @@ $content_info_title1 = "쉽고 강력합니다.";
 $content_info_title2 = "Keka는 당신의 개인 정보를 안전하게 압축합니다.";
 $content_info_title3 = "압축 파일의 용량이 너무 커서 공유할 수 없나요?";
 $content_info_text1 = "압축하기 위해 Keka를 실행할 필요가 없습니다.<br />파일과 폴더를 Dock 아이콘 또는 Keka 창에 끌어 놓아서, 압축 파일을 생성하세요.";
-$content_info_text2 = "암호화된 압축 파일을 안전하게 공유하세요.<br />Keka를 사용하시면, 7z 파일을 위한 AES-256 암호화 방식과<br />Zip 2.0 암호화 방식을 사용할 수 있습니다.";
+$content_info_text2 = "암호화된 압축 파일을 안전하게 공유하세요.<br />Keka를 사용하시면, 7z 파일을 위한 AES-256 암호화 방식과<br />Zip 2.0 레거시 암호화 방식을 사용할 수 있습니다.";
 $content_info_text3 = "만약 파일 크기가 너무 커서 이메일 첨부 또는 서버 업로드가 불가능하다면, <br />분할 압축을 해보세요.<br />압축 해제 시 다시 원본 파일로 복구할 수 있습니다.";
 $content_info_compression = "Keka에서 압축을 지원하는 파일 형식";
 $content_info_extraction = "Keka에서 압축 해제를 지원하는 파일 형식";
@@ -73,13 +73,13 @@ $content_info_v2_title4 = "멀티태스킹 최적화";
 $content_info_v2_title5 = "어디서든 편리하게";
 $content_info_v2_text1 = "다양한 압축 형식 중에서<br />꼭 맞는 방식을 선택하세요";
 $content_info_v2_text2 = "공유할 파일을 비밀번호로 보호하고<br />AES-256으로 암호화하세요";
-$content_info_v2_text3 = "내가 원하는 것만<br />미리보고, 압축하고, 공유하세요";
+$content_info_v2_text3 = "내가 원하는 것만<br />미리보고, 압축 해제하고, 공유하세요";
 $content_info_v2_text4 = "제한 없이<br />압축, 압축 해제 및 탐색하세요";
 $content_info_v2_text5 = "어디서나 공유 동작으로<br />압축, 압축 해제 및 탐색하세요";
 
 // Default app
 $content_defaultapp_title = "Keka를 기본 압축 앱으로 설정해주세요";
-$content_defaultapp_text = '<a href="https://github.com/aonez/Keka/wiki/Default-application">도움말<i class="far fa-question-circle"></i></a>을 이용해서 간단하게 Keka를 설치하고, <br />데스크톱 앱의 기본 압축 앱으로 설정해보세요.';
+$content_defaultapp_text = 'Keka를 기본 압축 해제 앱으로 설정하려면 <a href="https://github.com/aonez/Keka/wiki/Default-application">도우미 앱이 필요합니다 <i class="far fa-question-circle"></i></a>.<br />도우미 앱을 다운로드하여 압축을 푼 뒤, 데스크톱으로 이동시켜 실행해 주세요.';
 
 // Terms of use
 $content_termsofuse_title = '이용약관';
@@ -88,7 +88,7 @@ $content_termsofuse_inapp_title = '앱 내 구입';
 $content_termsofuse_inapp = 'Keka App Store 버전에서는 앱 내 구입을 통해 <a href="https://www.keka.io/#lovekeka">PayPal/웹사이트 내 스폰서</a>처럼 프로젝트 개발을 독려하고 지원할 수 있습니다. 결제는 선택 사항이며 추가로 제공되는 기능은 없습니다.';
 
 // Privacy Policy
-$content_privacypolicy_title = '개인정보취급방침';
+$content_privacypolicy_title = '개인정보처리방침';
 $content_privacypolicy_content = 'Keka는 사용자의 컴퓨터에서 어떤 데이터도 보내거나 저장하지 않습니다.';
 $content_privacypolicy_MAS = 'MAS 버전에는 네트워크 기능이 일절 없습니다.';
 $content_privacypolicy_WEB = 'WEB 버전은 업데이트 과정에서 Sparkle을 이용해 (비활성화 가능), 구성 요소가 인터넷에 연결할 수 있도록 합니다. 이는 새 버전을 확인하고 다운로드하는 용도로만 쓰입니다. 본 구성 요소는 어떤 정보도 보내거나 저장하지 않으며, 심지어 <a href="https://sparkle-project.org/documentation/system-profiling/">익명 시스템 프로필 데이터</a>조차 Keka 1.0.0 이전 일부 버전에서 설정이 켜진 적 있지만 절대 실제로 쓰이지는 않았습니다.';
